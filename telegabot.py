57662456dbc889628a3ce7c956ee7f8ff30e684b
CHFEED <fordirt@bk.ru>
2018-10-06 11:52:45 +0300

    d

--- telegabot.py
+++ telegabot.py
@@ -50,20 +50,21 @@ def main():
         last_chat_text = last_update['message']['text']
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
+        person_type=last_update['is_bot']
+        if person_type in gretings == false
+            if last_chat_text.lower() in greetings  and 6 <= hour < 12:
+                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+                today += 1
 
-        if last_chat_text.lower() in greetings  and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
+            elif last_chat_text.lower() in greetings  and 12 <= hour < 17:
+                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+                today += 1
 
-        elif last_chat_text.lower() in greetings  and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
+            elif last_chat_text.lower() in greetings  and 17 <= hour < 23:
+                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+                today += 1
 
-        elif last_chat_text.lower() in greetings  and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
+            new_offset = last_update_id + 1
 
 if __name__ == '__main__':  
     try:
